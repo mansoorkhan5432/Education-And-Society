e66dba6dd2f72c7063dab3fdf35fdbf55bbe2332
Mansoor Khan <114287813+mansoorkhan5432@users.noreply.github.com>
2023-02-02 17:10:09 +0500

    Add files via upload

---
+++ PSDhub/recources/js/psdscript.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+   $('.js-section-department').waypoint(function(direction) {
+       if(direction == "down") {
+           $('nav').addClass('sticky');
+       } else {
+           $('nav').removeClass('sticky');
+       }
+   }, {
+       offset: '230px;'
+   });
+   
+//    navigation scroll
+    
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  }); 
+    
+    $('.js-nav-icon').click(function() {
+        var nav = $('.js-main-nav');
+        var icon = $('.js-nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('fas fa-bars')) {
+            icon.addClass('far fa-times-circle');
+            icon.removeClass('fas fa-bars');
+        } else {
+            icon.addClass('fas fa-bars');
+            icon.removeClass('far fa-times-circle');
+        }
+    });
+    
+//    animation on scroll
+    $('.js-wp-1').waypoint(function(direction) {
+        $('.js-wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '60%'
+    });
+    
+    
+    
+}); 
